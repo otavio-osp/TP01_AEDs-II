88da4425db3154839218c65bb9dcfadc401766b0
Tomás Viana <tomasvianarf@gmail.com>
2026-06-01 22:18:35 -0300

    implementação inicial do pesquisa e do README

--- Pesquisa/pesquisa.h
+++ Pesquisa/pesquisa.h
@@ -4,8 +4,39 @@
 #ifndef PESQUISA_H
 #define PESQUISA_H
 
-#include "TADLista/lista.h"
+#include "../TADLista/lista.h"     
+#include "../TADHash/hash.h"       
+#include "../TADPatricia/patricia.h" 
 
-// Assinaturas para algoritmos de cálculo TF-IDF e ranqueamento de relevância.
+// Estrutura auxiliar para armazenar a pontuação de relevância r(i) de um documento. 
+// Será usada para ordenar os resultados no final.
 
-#endif // PESQUISA_H
+typedef struct {
+    int idDoc;
+    float relevancia;
+} DocumentoRelevancia;
+
+
+// Funçãp principal de pesquisa (chamada no caso 4 do main).
+
+
+void realizar_busca(char* consulta, TabelaHash tabela, TipoArvore raiz, int N, int* n_i);
+// Recebe os termos de busca, pesquisa nos índices e imprime os documentos em ordem de relevância.
+
+// (Cálculo do TF-IDF)
+
+// Calcula o peso (w_j,i) de um termo em um documento específico.
+// Fórmula: w_j,i = f_j,i * (log2(N) / d_j)
+float calcular_peso_w(int f_ji, int N, int d_j);
+
+
+// Calcula a relevância final r(i) de um documento para a consulta inteira.
+// Fórmula: r(i) = (1 / n_i) * somatorio(w_j,i)
+float calcular_relevancia_r(int n_i, float somatorio_pesos);
+
+//Funções Auxiliares
+
+// Ordena o vetor de DocumentoRelevancia de forma DECRESCENTE (maior relevância primeiro).
+void ordenar_resultados_por_relevancia(DocumentoRelevancia* resultados, int tamanho);
+
+#endif
